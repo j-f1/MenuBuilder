a9f902727eaceef404f22bfa3b3da3969e3bfb78
Erk Ekin <erkekin@users.noreply.github.com>
2021-03-13 23:02:31 +0000

    Swift 5.3.2 support (#3)
    Co-authored-by: Jed Fox <git@jedfox.com>

--- Sources/MenuBuilder Demo/MenuBuilder Demo/ViewController.swift
+++ Sources/MenuBuilder Demo/MenuBuilder Demo/ViewController.swift
@@ -31,10 +31,10 @@ class ViewController: NSViewController {
             MenuItem("Quit")
                 .shortcut("q")
                 .onSelect { NSApp.terminate(nil) }
-
-            for word in ["Hello", "World"] {
-                MenuItem(word)
-            }
+            // Uncomment once Xcode 12.5 becomes available
+            // for word in ["Hello", "World"] {
+            //     MenuItem(word)
+            // }
         }
     }
 
@@ -51,7 +51,7 @@ struct MyMenuItemView: View {
     var body: some View {
         HStack {
             Slider(value: $value, in: 0...9, step: 1)
-            Text("Value: \(Int(value))").font(Font.body.monospacedDigit())
+            Text("Value: \(value)").font(Font.body.monospacedDigit())
         }
         .padding(.horizontal)
         .frame(minWidth: 250)
